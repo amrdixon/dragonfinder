ed872d6d0903d5496dbf71218922c8436ab5cc09
Dixon <annamrdixon@gmail.com>
2021-01-19 11:41:30 -0500

    Modified AWS deployment script and added requirements.txt file.

--- cloud_deployment/aws_model_deploy.py
+++ cloud_deployment/aws_model_deploy.py
@@ -1,31 +1,30 @@
-from sagemaker.tensorflow import TensorFlowModel, predictor
+from sagemaker.tensorflow.model import TensorFlowModel
 import boto3
 import sagemaker
 
-
-
-class SageMakerTFInference:
+def deploy_smtf_endpoint(model_data_s3=None, iam_arn=None, framework_version='2.3.0'):
+    """
+    Deploys TensorFlow model to SageMaker endpoint
     
-    def __init__(self, sm_inference_arn=None, model_data_s3=None, iam_arn=None, framework_version='2.3.0'):
+    args:
+    model_data_s3 (str, defaul None): S3 URI for compressed TF model to be deployed
+    iam_arn (str, default None): AWS IAM ARN with appropriate SageMaker and S3 permissions
+    framework_version (str, default '2.3.0'): TensorFlow Serving version
+    
+    returns:
+    SageMaker Endpoint ARN (str)
+    
+    """
+    
+    try:
+        model = TensorFlowModel(model_data=model_data_s3,\
+        role=iam_arn,\
+        framework_version=framework_version)
+        #TODO: add option to specify deploy kwargs
+        predictor = model.deploy(initial_instance_count=1, instance_type='ml.c5.xlarge')
+        sm_inference_arn = predictor.endpoint_name
+        print('Successfully deployed Sagemaker TF inference endpoint to: {}'.format(sm_inference_arn))
+    except Exception as e:
+        print("Unable to deploy Sagemaker TF model inference endpoint: {}".format(str(e)))
         
-        #if calling existing sagemaker tf endpoint
-        if sm_inference_arn:
-            try:
-                self._sm_inference_arn = sm_inference_arn
-                self._predictor = predictor.Predictor(sm_inference_arn)
-                print("Connection to Sagemaker TF inference endpoint successful.")
-            except Excetption as e:
-                print("Connection to Sagemaker TF inference endpoint failed: {}".format(str(e)))
-        else:
-            try:
-                model = TensorFlowModel(model_data=model_data_s3,\
-                role=iam_arn,\
-                framework_version=framework_version)
-                #TODO: add option to specify deploy kwargs
-                self._predictor = model.deploy(initial_instance_count=1, instance_type='ml.c5.xlarge')
-                self._sm_inference_arn = self._predictor.endpoint_name
-                print('Successfully deployed Sagemaker TF inference endpoint to: {}'.format(self._sm_inference_arn))
-            except Exception as e:
-                print("Unable to deploy Sagemaker TF model inference endpoint: {}".format(str(e)))
-                
-                
+    return sm_inference_arn
