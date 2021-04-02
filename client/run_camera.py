2e3fc905d9a1bcec790d1d3988ef9a2852a99591
Dixon <annamrdixon@gmail.com>
2021-04-02 09:56:21 -0400

    Added picamera demo.

---
+++ client/run_camera.py
@@ -0,0 +1,10 @@
+import time
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+	camera.annotate_text = 'Hello world!'
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('picamera_image.jpg')
