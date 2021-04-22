07b5c0e1a2a42bb30be1e419bfe155d0b40d8eb7
amrdixon <annamrogers@gmail.com>
2021-04-22 06:04:08 -0400

    Updating to make API work on pi.

--- client/run_camera.py
+++ client/run_camera.py
@@ -4,7 +4,7 @@ import picamera
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.start_preview()
-	camera.annotate_text = 'Hello world!'
+	#camera.annotate_text = 'Hello world!'
     # Camera warm-up time
     time.sleep(2)
     camera.capture('picamera_image.jpg')
