c137e5c0285043f44f2d079add54ec18a0b96669
Dixon <annamrdixon@gmail.com>
2021-04-22 15:07:53 -0400

    Fixed bug in run_dragon_finder_pi script and added some documentation.

--- run_dragon_finder_pi.py
+++ run_dragon_finder_pi.py
@@ -2,21 +2,39 @@ import time
 import picamera
 import requests
 import json
+import logging
 
 
 def capture_image(img_filepath='picamera_image.jpg'):
+    """
+    Starts pi camera, takes photo and saves to file
+    
+    args:
+    img_filepath (str, default='picamera_image.jpg'): filepath to write
+    returns:
+    img_filepath
+    
+    """
     
     with picamera.PiCamera() as camera:
         camera.resolution = (1024, 768)
         camera.start_preview()
-    	#camera.annotate_text = 'Hello world!'
-        # Camera warm-up time
         time.sleep(2)
         camera.capture('picamera_image.jpg')
         
     return img_filepath
         
 def run_inference_restapi(img_filepath):
+    """
+    Runs inference using local Docker client running on pi
+    args:
+    img_filepath (str): filepath to local image to run inference on
+    returns:
+    prediction (str)
+    
+    """
+    
+    
      headers = {"content-type": "multipart/form-data"}
      local_client_url = 'http://0.0.0.0:5000/classify?server_type=RestAPIPublic'
      x = requests.post(local_client_url, data=img_filepath, headers=headers)
